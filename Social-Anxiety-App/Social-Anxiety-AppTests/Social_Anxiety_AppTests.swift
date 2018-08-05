8abd8db8a333b13af2b5146546bac27d1f9ded7d
Charybdiss <ktmlaing@gmail.com>
2018-08-05 15:31:23 +1200

    Succeeded in making a project. Builds and runs all features. First working version of new git

---
+++ Social-Anxiety-App/Social-Anxiety-AppTests/Social_Anxiety_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Social_Anxiety_AppTests.swift
+//  Social-Anxiety-AppTests
+//
+//  Created by Nathan Laing on 8/5/18.
+//  Copyright © 2018 Nathan Laing. All rights reserved.
+//
+
+import XCTest
+@testable import Social_Anxiety_App
+
+class Social_Anxiety_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
